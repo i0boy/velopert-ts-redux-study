00c2183f07fae4fad52c9c9e6e1e3c44a032490d
ith13579 <ith13579@korea.ac.kr>
2021-01-14 08:29:07 +0900

    사가 리팩토링 끝

--- src/modules/github/actions.ts
+++ src/modules/github/actions.ts
@@ -6,6 +6,7 @@ export const GET_USER_PROFILE = "github/GET_USER_PROFILE";
 export const GET_USER_PROFILE_SUCCESS = "github/GET_USER_PROFILE_SUCCESS";
 export const GET_USER_PROFILE_ERROR = "github/GET_USER_PROFILE_ERROR";
 
+// 로딩 중 액션 페이로드 string
 export const getUserProfileAsync = createAsyncAction(
   GET_USER_PROFILE,
   GET_USER_PROFILE_SUCCESS,
