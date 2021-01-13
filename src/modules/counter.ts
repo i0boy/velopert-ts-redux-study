c1aff06ab0a7831ecd7791209b83e5789d6db4dd
ith13579 <ith13579@korea.ac.kr>
2021-01-13 17:15:37 +0900

    리덕스 타입세이프 액션 리팩토링 완료

--- src/modules/counter.ts
+++ src/modules/counter.ts
@@ -3,36 +3,15 @@
 // 뒤에 as const 를 붙여줌으로써 나중에 액션 객체를 만들게 action.type 의 값을 추론하는 과정에서
 // action.type 이 string 으로 추론되지 않고 'counter/INCREASE' 와 같이 실제 문자열 값으로 추론 되도록 해줍니다.
 
-const INCREASE = "counter/INCREASE" as const;
-const DECREASE = "counter/DECREASE" as const;
-const INCREASE_BY = "counter/INCREASE_BY" as const;
+import { createAction, ActionType, createReducer } from "typesafe-actions";
 
-// 액션 생성함수를 선언합니다.
-export const increase = () => ({
-  type: INCREASE,
-});
+// 액션 생성함수를 선언합니다
+export const increase = createAction("counter/INCREASE")();
+export const decrease = createAction("counter/DECREASE")();
+export const increaseBy = createAction("counter/INCREASE_BY")<number>(); // payload 타입을 Generics 로 설정해주세요.
 
-export const decrease = () => ({
-  type: DECREASE,
-});
-
-// 액션에 부가적으로 필요한 값을 payload 라는 이름으로 통일합니다
-// 이는 FSA (https://github.com/redux-utilities/flux-standard-action) 라는 규칙인데
-// 이 규칙을 적용하면 액션들이 모두 비슷한 구조로 이루어져있게 되어 추후 다룰 때도 편하고
-// 읽기 쉽고, 액션 구조를 일반화함으로써 액션에 관련돤 라이브러리를 사용 할 수 있게 해줍니다.
-// 다만, 무조건 꼭 따를 필요는 없습니다.
-export const increaseBy = (diff: number) => ({
-  type: INCREASE_BY,
-  payload: diff,
-});
-
-// 모든 액션 겍체들에 대한 타입을 준비해줍니다.
-// ReturnType<typeof _____> 는 특정 함수의 반환값을 추론해줍니다
-// 상단부에서 액션타입을 선언 할 떄 as const 를 하지 않으면 이 부분이 제대로 작동하지 않습니다.
-type CounterAction =
-  | ReturnType<typeof increase>
-  | ReturnType<typeof decrease>
-  | ReturnType<typeof increaseBy>;
+const actions = { increase, decrease, increaseBy };
+type CounterAction = ActionType<typeof actions>;
 
 // 이 리덕스 모듈에서 관리 할 상태의 타입을 선언합니다
 type CounterState = {
@@ -44,19 +23,14 @@ const initialState: CounterState = {
   count: 0,
 };
 
-// 리듀서
-export default function counter(
-  state: CounterState = initialState,
-  action: CounterAction
-): CounterState {
-  switch (action.type) {
-    case INCREASE:
-      return { count: state.count + 1 };
-    case DECREASE:
-      return { count: state.count - 1 };
-    case INCREASE_BY:
-      return { count: state.count + action.payload };
-    default:
-      return state;
-  }
-}
+// 리듀서를 만듭니다
+// createReducer 는 리듀서를 쉽게 만들 수 있게 해주는 함수입니다.
+// Generics로 리듀서에서 관리할 상태, 그리고 리듀서에서 처리 할 모든 액션 객체들의 타입을 넣어야합니다
+const counter = createReducer<CounterState, CounterAction>(initialState)
+  .handleAction(increase, (state) => ({ count: state.count + 1 }))
+  .handleAction(decrease, (state) => ({ count: state.count - 1 }))
+  .handleAction(increaseBy, (state, action) => ({
+    count: state.count + action.payload,
+  }));
+
+export default counter;
--- src/modules/todos.ts
+++ src/modules/todos.ts
@@ -1,43 +1,37 @@
-// Ducks 패턴  즉, 액션타입, 액션생성함수, 리듀서를 모두 한 파일에 작성하겠다는 의미이죠.
-// 액션 타입을 선언합니다
-// 뒤에 as const 를 붙여줌으로써 나중에 액션 객체를 만들게 action.type 의 값을 추론하는 과정에서
-// action.type 이 string 으로 추론되지 않고 'counter/INCREASE' 와 같이 실제 문자열 값으로 추론 되도록 해줍니다.
-const ADD_TODO = "todos/ADD_TODO" as const;
-const TOGGLE_TODO = "todos/TOGGLE_TODO" as const;
-const REMOVE_TODO = "todos/REMOVE_TODO" as const;
+import {
+  /* action, */
+  createAction,
+  ActionType,
+  createReducer,
+} from "typesafe-actions";
 
-let nextId: number = 1; // 새로운 항목을 추가 할 때 사용 할 고유 ID 값
+let nextId = 1; // 새로운 항목을 추가 할 때 사용 할 고유 ID 값
+
 // 액션 생성 함수
-export const addTodo = (text: string) => ({
-  type: ADD_TODO,
-  payload: {
-    id: nextId++,
-    text,
-  },
-});
 
-// 액션에 부가적으로 필요한 값을 payload 라는 이름으로 통일합니다
-// 이는 FSA (https://github.com/redux-utilities/flux-standard-action) 라는 규칙인데
-// 이 규칙을 적용하면 액션들이 모두 비슷한 구조로 이루어져있게 되어 추후 다룰 때도 편하고
-// 읽기 쉽고, 액션 구조를 일반화함으로써 액션에 관련돤 라이브러리를 사용 할 수 있게 해줍니다.
-// 다만, 무조건 꼭 따를 필요는 없습니다.
-export const toggleTodo = (id: number) => ({
-  type: TOGGLE_TODO,
-  payload: id,
-});
+// 이 액션 생성 함수의 경우엔 파라미터를 기반하여 커스터마이징된 payload를 설정해주므로,
+// createAction 이라는 함수를 사용합니다.
+// 여기서 action은 액션 객체를 만드는 함수입니다
+export const addTodo = createAction("todos/ADD_TODO", (text: string) => ({
+  id: nextId++,
+  text: text,
+  done: false,
+}))<Todo>();
+// 위 코드는 다음과 같은 형태로도 구현 할 수 있지만, createAction 말고 action 만 사용하면
+// Action Helpers (https://www.npmjs.com/package/typesafe-actions#action-helpers-api) 지원이 안됩니다.
+// export const addTodo = (text: string) => action(ADD_TODO, { id: nextId++, text })
 
-export const removeTodo = (id: number) => ({
-  type: REMOVE_TODO,
-  payload: id,
-});
+// payload가 그대로 들어가는 액션생성함수는 정말 간단합니다.
+export const toggleTodo = createAction("todos/TOGGLE_TODO")<number>();
+export const removeTodo = createAction("todos/REMOVE_TODO")<number>();
 
-// 모든 액션 겍체들에 대한 타입을 준비해줍니다.
-// ReturnType<typeof _____> 는 특정 함수의 반환값을 추론해줍니다
-// 상단부에서 액션타입을 선언 할 떄 as const 를 하지 않으면 이 부분이 제대로 작동하지 않습니다.
-type TodosAction =
-  | ReturnType<typeof addTodo>
-  | ReturnType<typeof toggleTodo>
-  | ReturnType<typeof removeTodo>;
+// 모든 액션 객체들에 대한 타입 준비
+const actions = {
+  addTodo,
+  toggleTodo,
+  removeTodo,
+};
+type TodosAction = ActionType<typeof actions>;
 
 // 상태에서 사용 할 할 일 항목 데이터 타입 정의
 export type Todo = {
@@ -49,31 +43,30 @@ export type Todo = {
 // 이 모듈에서 관리할 상태는 Todo 객체로 이루어진 배열
 export type TodosState = Todo[];
 
-// 초기 상태 선언 (투두배열임)
+// 초기 상태 선언
 const initialState: TodosState = [];
 
 // 리듀서 작성
-export default function todos(
-  state: TodosState = initialState,
-  action: TodosAction
-): TodosState {
-  switch (action.type) {
-    case ADD_TODO:
-      return state.concat({
-        // action.payload 객체 안의 값이 모두 유추됩니다.
-        id: action.payload.id,
-        text: action.payload.text,
-        done: false,
-      });
-    case TOGGLE_TODO:
-      return state.map((todo) =>
-        // payload 가 number 인 것이 유추됩니다.
-        todo.id === action.payload ? { ...todo, done: !todo.done } : todo
-      );
-    case REMOVE_TODO:
-      // payload 가 number 인 것이 유추됩니다.
-      return state.filter((todo) => todo.id !== action.payload);
-    default:
-      return state;
-  }
-}
+const todos = createReducer<TodosState, TodosAction>(initialState)
+  .handleAction(removeTodo, (state, action) =>
+    state.filter((todo) => todo.id !== action.payload)
+  )
+  .handleAction(toggleTodo, (state, action) =>
+    state.map((todo) =>
+      todo.id === action.payload ? { ...todo, done: !todo.done } : todo
+    )
+  )
+  .handleAction(addTodo, (state, action) =>
+    state.concat({ ...action.payload })
+  );
+
+export default todos;
+
+// const counter = createReducer<CounterState, CounterAction>(initialState)
+//   .handleAction(increase, (state) => ({ count: state.count + 1 }))
+//   .handleAction(decrease, (state) => ({ count: state.count - 1 }))
+//   .handleAction(increaseBy, (state, action) => ({
+//     count: state.count + action.payload,
+//   }));
+
+// export default counter;
